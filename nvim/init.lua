c8b09c3dba86393f4c190819be55ae38b199b84b
João Veiga <styluss@gmail.com>
2021-07-06 10:44:07 +0200

    neovim lua

---
+++ nvim/init.lua
@@ -0,0 +1,75 @@
+vim.cmd [[packadd vim-packager]]
+require('packager').setup(function(packager)
+	packager.add('phaazon/hop.nvim')	
+	packager.add('itchyny/landscape.vim')
+	packager.add('nvim-lua/lsp_extensions.nvim')
+	packager.add('neovim/nvim-lspconfig')
+	packager.add('nvim-lua/completion-nvim')
+end)
+
+vim.g.colors_name = 'landscape'
+
+-- Enable diagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = true,
+    signs = true,
+    update_in_insert = true,
+  }
+)
+
+function goimports(timeout_ms)
+	local context = { source = { organizeImports = true } }
+	vim.validate { context = { context, "t", true } }
+
+	local params = vim.lsp.util.make_range_params()
+	params.context = context
+
+	-- See the implementation of the textDocument/codeAction callback
+	-- (lua/vim/lsp/handler.lua) for how to do this properly.
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+	if not result or next(result) == nil then return end
+	local actions = result[1].result
+	if not actions then return end
+	local action = actions[1]
+
+	-- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+	-- is a CodeAction, it can have either an edit, a command or both. Edits
+	-- should be executed first.
+	if action.edit or type(action.command) == "table" then
+		if action.edit then
+			vim.lsp.util.apply_workspace_edit(action.edit)
+		end
+		if type(action.command) == "table" then
+			vim.lsp.buf.execute_command(action.command)
+		end
+	else
+		vim.lsp.buf.execute_command(action)
+	end
+end
+
+-- autocmd BufWritePre *.go lua goimports(1000)
+local nvim_lsp = require'lspconfig'
+
+-- function to attach completion when setting up lsp
+local on_attach = function(client)
+    require'completion'.on_attach(client)
+end
+
+-- Enable rust_analyzer
+nvim_lsp.rust_analyzer.setup({ on_attach=on_attach })
+
+nvim_lsp.gopls.setup {
+	cmd = {"gopls", "serve"},
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+		},
+	},
+}
+
+vim.api.nvim_set_keymap('n', '\\$', "<cmd>lua require'hop'.hint_words()<cr>", {})
+vim.opt.number=true
--- vim/ftplugin/tex.vim
+++
@@ -1,3 +0,0 @@
-map ,h : !pdflatex main.tex <CR> <CR>
-set spell
-set spelllang=en,pt
